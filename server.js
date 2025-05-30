0167842ba890f986fb6226422999c57108651142
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-05-30 19:31:28 +0300

    solve port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import http from 'http';
 import { setupSocket } from './src/config/socket.js';
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app)
 
